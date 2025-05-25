2af5027dea292b9684b533b9ed5eca403009a19c
kenne <kenneth.nalvarte.1@gmail.com>
2025-05-24 20:42:17 -0600

    Add /login and /signup routes to enable navigation from homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
