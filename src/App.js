8b020976571dbe2555664ddb2997c5b522e97fdb
kenne <kenneth.nalvarte.1@gmail.com>
2025-05-28 11:02:27 -0600

    chore: trigger Netlify redeploy with trivial comment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Trigger Netlify redeploy: trivial change
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
