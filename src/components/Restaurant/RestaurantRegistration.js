f83a1570f1fa59557d9e57e0acc4197cf777ac48
kenne <kenneth.nalvarte.1@gmail.com>
2025-05-31 08:04:02 -0600

    Remove duplicate 'location' declaration in handleSubmit (fix SyntaxError)

--- src/components/Restaurant/RestaurantRegistration.js
+++ src/components/Restaurant/RestaurantRegistration.js
@@ -68,7 +68,7 @@ const RestaurantRegistration = () => {
         return;
       }
       // TEMP: Bypass geocoding/location for testing
-      const location = { latitude: 0, longitude: 0 };
+      // const location = { latitude: 0, longitude: 0 };
       // Create restaurant document
       let logoUrl = '';
       if (formData.logo) {
